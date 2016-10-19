336d5b49c4eed33fefa1a9d1ce99f314d7deabcb
James Christopher Adduono <jc@adduono.com>
2016-10-19 01:39:01 -0400

    Use API 21
    Change-Id: I2daf63376371eb7fd8a85f9219c615a4e0ceaeb4

--- Android.mk
+++ Android.mk
@@ -3,6 +3,5 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := hid-keyboard
 LOCAL_SRC_FILES := hid-gadget-test.c
-LOCAL_CFLAGS += -fPIE
-LOCAL_LDFLAGS += -fPIE -pie
+LOCAL_SDK_VERSION := 21
 include $(BUILD_EXECUTABLE)
